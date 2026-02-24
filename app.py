ba8107fda382fc856c16aab7c881cb12e02ef83b
Hemasri <mulavagilahemasrirenuka@gmail.com>
2026-02-24 09:34:52 +0530

    modified flask post behaviour

--- app.py
+++ app.py
@@ -9,17 +9,25 @@ model = pickle.load(open("gold_model.pkl", "rb"))
 @app.route("/", methods=["GET","POST"])
 def home():
     prediction = None
+    spx = oil = slv = eurusd = ""
 
     if request.method == "POST":
-        spx = float(request.form["spx"])
-        oil = float(request.form["oil"])
-        slv = float(request.form["slv"])
-        eurusd = float(request.form["eurusd"])
+        spx = request.form["spx"]
+        oil = request.form["oil"]
+        slv = request.form["slv"]
+        eurusd = request.form["eurusd"]
 
-        data = np.array([[spx, oil, slv, eurusd]])
+        data = np.array([[float(spx), float(oil), float(slv), float(eurusd)]])
         prediction = model.predict(data)[0]
 
-    return render_template("index.html", prediction=prediction)
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        spx=spx,
+        oil=oil,
+        slv=slv,
+        eurusd=eurusd
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
